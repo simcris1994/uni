830b215ca5c91268004713a99cbaab3148b11a77
Cristina <cristina_simionescu@bitbucket.org>
2017-11-13 10:30:08 +0100

    progress?

--- programming_paradigms/miniProj2.hs
+++ programming_paradigms/miniProj2.hs
@@ -22,24 +22,56 @@ sine a = (Sin (Pol (Var a)))
 cosine a = (Cos (Pol (Var a)))
 sin2 a = (Mult (sine a) (sine a))
 cos2 a = (Mult (cosine a) (cosine a))
+numConst a x = (Pol (Times a (Power x 0)))
 
 -- examples to test on
 ex1 = Mult (powerOf "x" 2) (powerOf "x" 3)
 ex2 = Divide (powerOf "x" 6) (powerOf "x" 3)
-ex3 = (Add (sin2 "x") (cos2 "x"))
+ex3 = Add (sin2 "x") (cos2 "x")
+ex4 = Mult (Pol (Var "x")) (Pol (Var "x"))
+ex5 = (Add (Add (powerOf "x" 7) (powerOf "x" 3)) (numConst 7 "x"))
+ex6 = (Mult (Mult (powerOf "x" 2) (powerOf "x" 4)) (powerOf "x" 3))
+-- ex7
+ex8 = (Mult (powerOf "x" 5) (numConst 1 "x"))
 
+-- functions for evaluating an expression, uses pattern matching to cover different cases
 
--- functions for evaluating an expression, uses pattern matching to cover different
--- situations
 -- x^m * x^n = x^(m+n)
 eval (Mult (Pol (Power x ex1)) (Pol (Power y ex2)))
     | x == y    =   (Pol (Power x (ex1 + ex2)))
     | otherwise =   (Mult (Pol (Power x ex1)) (Pol (Power y ex2)))
+    
 -- x^m / x^m = x^(m-n)
 eval (Divide (Pol (Power x ex1)) (Pol (Power y ex2)))
     | x == y    =   (Pol (Power x (ex1 - ex2)))
-    | otherwise = (Divide (Pol (Power x ex1)) (Pol (Power y ex2)))  
+    | otherwise = (Divide (Pol (Power x ex1)) (Pol (Power y ex2)))
+
 -- sin^2 x + cos^2 x = 1
 eval (Add (Mult (Sin (Pol (Var a))) (Sin (Pol (Var b)))) (Mult (Cos (Pol (Var x))) (Cos (Pol (Var y)))))
-    | x == y    =   (Pol (Var "1"))
+    | x == y    =   numConst 1 x
     | otherwise =  (Add (Mult (Sin (Pol (Var a))) (Sin (Pol (Var b)))) (Mult (Cos (Pol (Var x))) (Cos (Pol (Var y))))) 
+
+-- e1  + e2 = e2 + e1
+eval (Add x y) 
+    | x == y    =   (Mult (Pol (Var "2")) x)
+    | otherwise =   (Add y x)
+    
+-- e * e = e^2
+eval (Mult (Pol (Var x)) (Pol (Var y)))
+    | x == y    =   (Pol (Power x 2))
+    | otherwise =   (Mult (Pol (Var y)) (Pol (Var x)))
+    
+-- (e1 + e2) + e3 = e1 + (e2 + e3)
+eval (Add (Add x y) z) = (Add x (Add y z))
+
+-- e1 * e2 = e2 * e1
+eval (Mult a b) = (Mult b a)
+
+-- (e1 * e2) * e3 = e1 * (e2 * e3)
+eval (Mult (Mult x y) z) = (Mult x (Mult y z))
+
+-- e1 * (e2 + e3) = e1 * e2 + e1 * e3
+
+-- e1 * 1 = e1
+eval (Mult a (Pol (Power x 0))) = a
+
