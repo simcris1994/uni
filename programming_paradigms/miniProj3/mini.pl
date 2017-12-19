901ac3708309c90abe879b52cb52757c7b0bd6a5
Cristina <cristina_simionescu@bitbucket.org>
2017-12-20 00:12:20 +0200

    problem 5

--- programming_paradigms/miniProj3/mini.pl
+++ programming_paradigms/miniProj3/mini.pl
@@ -16,25 +16,38 @@ visaAgreement(countryA, countryB)
 operates(airline, leg)
 */
 
-countryOfAirport(cph, denmark).
-countryOfAirport(lax, usa).
+
+airport(cph, copenhagen, denmark, clear).
+airport(lax, los_angeles, usa, clear).
+airport(jfk, new_york_city, usa, clear).
+
 passport(jane, sweden).
 passport(mike, usa).
-reservedBy(abc123, jane).
-reservedBy(abd124, jane).
-reservedBy(bbb123, mike).
-reservationDestination(abc123, cph).
-reservationDestination(abd124, lax).
-reservationDestination(bbb123, cph).
+
+leg(jfk, cph, klm, sas, boeing).
+leg(jfk, lax, norwegian, sas, boeing).
+
+reservation(abc123, jane, jfk, cph, sas, a18).
+reservation(abd124, jane, jfk, lax, sas, f7).
+reservation(bbb123, mike, jfk, cph, sas, c20).
+reservation(bbb124, mike, jfk, cph, sas, a18).
+
 visaAgreement(sweden, denmark).
+visaAgreement(usa, denmark).
 
 
 canFlyInto(Passenger, Airport) :- passport(Passenger, Country),
-	countryOfAirport(Airport, Country).
+	airport(Airport, _, Country, _).
 canFlyInto(Passenger, Airport) :- visaAgreement(PassengerCountry, AirportCountry),
 	passport(Passenger, PassengerCountry),
-	countryOfAirport(Airport, AirportCountry).
+	airport(Airport, _, AirportCountry, _).
 
-illegalReservation(Passenger, Reservation) :- reservedBy(Reservation, Passenger),
-	reservationDestination(Reservation, Airport),
+illegalReservation(Passenger, Code) :- reservation(Code, Passenger, _, Airport, _, _),
 	not(canFlyInto(Passenger, Airport)).
+	
+doubleBooking(Booking) :- reservation(Booking, Passenger1, OriginAirport, DestinationAirport, OperatorAirline, SeatNo),
+	reservation(Booking2, Passenger2, OriginAirport, DestinationAirport, OperatorAirline, SeatNo),
+	leg(OriginAirport, DestinationAirport, _, OperatorAirline, _),
+	Passenger1 \= Passenger2,
+	Booking \= Booking2.
+	
